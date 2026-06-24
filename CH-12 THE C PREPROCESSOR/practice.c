0948429ccc9f2bf2f4f502f0996e2bcb89543433
TharunPranav125 <85447896+TharunPranav125@users.noreply.github.com>
2026-06-24 09:28:28 +0530

    Practice

--- CH-12 THE C PREPROCESSOR/practice.c
+++ CH-12 THE C PREPROCESSOR/practice.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 
+#define AREA(x)(3.14*x*x)
+
+int main(){
+    float r1 = 6.25,r2 = 2.5,a;
+    a = AREA(r1);
+    printf("Area : %f",a);
+    a = AREA(r2);
+    printf("Area : %f",a);
+    return 0;
+}
